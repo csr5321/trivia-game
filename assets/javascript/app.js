6fc6644dcf5b90dbd203478d6f406d9e4b809d3b
Courtney Radford <Courtney6554@Courtneys-MBP.attlocal.net>
2017-08-24 23:30:29 -0700

    final comm, buggy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,72 @@
 window.onload = function() {
 
-  $('#quizCard').hide();
-  $('#score').hide();
-  $('#submit').hide();
-  $('#play').show();
+    $('#quizCard').hide();
+    $('#score').hide();
+    $('#submit').hide();
+    $('#play').show();
 
-  $('#play').on("click", function () {
-    $('#quizCard').show();
-    $('#score').show();
-    $('#submit').show();
-    $('#play').hide();
-    timer.start();
+    $('#play').on("click", function() {
+        $('#quizCard').show();
+        $('#score').show();
+        $('#submit').show();
+        $('#play').hide();
+        timer.reset();
+        timer.start();
+        // could not clear inputs on the reset 
+        //suspect from the way i built it in quizCard. 
+        //Bad coding rabbithole.  
+        $("input[type=radio], textarea").val(""); 
 
-  })
+    })
 
     var intervalId;
     var clockRunning = false;
     var timer = {
-      time: 3600,
+        time: 3600,
 
-      reset: function () {
-        timer.time = 3600;
-      },
+        reset: function() {
+            timer.time = 3600;
+        },
 
-      start: function() {
-        if (!clockRunning) {
-          intervalId = setInterval(timer.count, 10)
+        start: function() {
+            if (!clockRunning) {
+                intervalId = setInterval(timer.count, 10)
+            }
+        },
+
+        stop: function() {
+            clearInterval(intervalId);
+        },
+
+        count: function() {
+            timer.time--;
+
+            displayTime = timer.timeConvert(timer.time);
+            $('#timer').html(displayTime);
+
+            if (timer.time === 00) {
+                timer.stop();
+                timer.reset();
+                $('#timer').html("Too Slow! Play Again.")
+                $('#play').show();
+                $('#quizCard').hide();
+                $('#submit').hide();
+            }
+        },
+
+        timeConvert: function(t) {
+            var seconds = Math.floor(t / 60);
+            var fracSeconds = t - (seconds * 60);
+            if (fracSeconds < 10) {
+                fracSeconds = "0" + fracSeconds;
+            }
+
+            if (seconds === 0) {
+                seconds = "00";
+            }
+
+            return seconds + ":" + fracSeconds;
         }
-      },
-
-      stop: function() {
-        clearInterval(intervalId);
-      },
-
-      count: function() {
-        timer.time--;
-
-        displayTime = timer.timeConvert(timer.time);
-        $('#timer').html(displayTime);
-
-        if (timer.time === 00) {
-          timer.stop();
-          timer.reset();
-          $('#timer').html("Too Slow! Play Again.")
-          $('#play').show();
-          $('#quizCard').hide();
-          $('#submit').hide();
-        }
-      },
-
-      timeConvert: function(t) {
-        var seconds = Math.floor(t/60);
-        var fracSeconds = t - (seconds * 60);
-          if (fracSeconds < 10) {
-            fracSeconds = "0" + fracSeconds;
-          }
-
-          if (seconds === 0) {
-            seconds = "00";
-          }
-
-          return seconds + ":" + fracSeconds;
-      }
     }
 
     var question;
@@ -70,8 +75,7 @@ window.onload = function() {
     var score;
     var quiz = {
 
-        characters: [
-            {
+        characters: [{
                 name: "Arya Stark",
                 fate: 1
             },
@@ -127,39 +131,44 @@ window.onload = function() {
             for (var i = 0; i < quiz.characters.length; i++) {
                 question = quiz.characters[i].name + " dies this season."
                 // console.log("Question #" + [i + 1] + ": " + question);
-                $('#quizCard').append("<div><b>" + [i + 1] + ". " + question + "</b></div><ul><input class='answer' type='radio' name='q" + [i + 1] + "' value='1'><label class='trueString'>True</label></ul><ul><input class='answer' type='radio' name='q" + [i + 1] + "' value='0'><label class='falseString'>False</label></ul>");
+                $('#quizCard').append("<div><b>" + [i + 1] + ". " + question + "</b></div><ul><input class='answer' id='select' type='radio' name='q" + [i + 1] + "' value='1'><label class='trueString'>True</label></ul><ul><input class='answer' id='select' type='radio' name='q" + [i + 1] + "' value='0'><label class='falseString'>False</label></ul>");
 
             }
 
         },
 
         scoreQ: function() {
-            for (var j = 0; j < quiz.characters.length; j++) {
-              var input = $('.answer').val();
-                if (quiz.characters[j].fate == input ) {
+            debugger
+            for (var j = 0; j < quiz.characters.length; j++)  {
+                if (quiz.characters[j].fate === $("input[type='radio']").val(0)) {
+                    correctAnswer++;
+
+                } else if (quiz.characters[j].fate === 0) {
                     correctAnswer++;
 
                 } else {
                     incorrectAnswer++;
 
                 }
-            }
 
-        },
+            }
+            console.log("There are " + correctAnswer + " correct answers.")
+            console.log("There are " + incorrectAnswer + " incorrect answers.")
+
+        }
 
 
     }
 
     quiz.populateQ(question);
+
     $('#submit').on("click", function() {
-    quiz.scoreQ();
-    timer.stop();
-    timer.reset();
+        timer.stop();
+        $('#play').show();
+        $('#quizCard').hide();
+        quiz.scoreQ();
 
-
-    console.log("There are " + correctAnswer + " correct answers.")
-    console.log("There are " + incorrectAnswer + " incorrect answers.")
-    $('#score').html("You answered " + correctAnswer + "/" + quiz.characters.length + " questions correctly. I won't spoil it for you!")
+        $('#score').html("You answered " + correctAnswer + "/" + quiz.characters.length + " questions correctly. No spoilers.")
 
     });
 
